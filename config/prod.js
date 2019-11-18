89caaaa404821c07a6d70bc56f86a64ce128b341
Trent Bechard <trent.bechard@gmail.com>
2019-11-18 14:19:56 +0800

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+//"mongodb+srv://differentuser:9RviUUb9xgr94yoj@emaily-prod-jhaf2.mongodb.net/test?retryWrites=true&w=majority"
